e807924b141557d0cb138439d0c0ad5cd9fb4a8d
DanielSchn <d.schneider@dsc-mail.de>
2024-07-10 16:51:00 +0200

    created new component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app"></div>
+      <BaseCard>
+      <h2>Card Title</h2>
+      <p>This is some content inside the card.</p>
+    </BaseCard>
+    <BaseCard>
+      <h2>Another Card</h2>
+      <p>Different content in another card.</p>
+    </BaseCard>
 </template>
 
 <script>
-
+import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
   components: {
-
+    BaseCard,
   }
 }
 </script>
@@ -16,11 +23,14 @@ export default {
 <style>
 body {
   margin: 0;
+ 
 }
 
 #app {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   width: 100vw;
   min-height: 100vh;
+  padding: 100px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,21 @@
+<!-- src/components/BaseCard.vue -->
+<template>
+    <div class="base-card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BaseCard',
+  };
+  </script>
+  
+  <style scoped>
+  .base-card {
+    background: #011F35;
+    border-radius: 16px;
+    padding: 24px 32px;
+  }
+  </style>
+  
