87861de776b1bcc9718e206f193e5ff8a29a9a78
DanielSchn <d.schneider@dsc-mail.de>
2024-07-11 15:43:15 +0200

    loadfunction for each stock

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,35 @@ export default {
     BaseCard,
   },
   async created() {
-    // this.data = await stockService.getRevenue('$AAPL');
-    this.data = await stockService.fetchData('$AAPL');
-    console.log('Loaded data', this.data);
+    const stocks = [
+      '$AAPL',
+      '$AMZN',
+      '$GOOG',
+      '$META',
+      '$MSFT',
+      '$NVDA',
+      '$TSLA'
+    ];
+
+    const stockData = [];
+
+
+
+    for (const stock of stocks) {
+      const data = await stockService.fetchData(stock);
+      const key = `data${stock.substring(1)}`;
+
+      if (!stockData[key]) {
+        stockData[key] = [];
+      }
+
+      stockData[key].push(data);
+      console.log(`Loaded Data for ${stock}`, data);
+    }
+
+    console.log('Data Stock', stockData);
+
+    return stockData;
   }
 }
 </script>
--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,9 +1,25 @@
 import axios from 'axios';
 
+const order = [
+  '1Q21',
+  '2Q21',
+  '3Q21',
+  '4Q21',
+  '1Q22',
+  '2Q22',
+  '3Q22',
+  '4Q22',
+  '1Q23',
+  '2Q23',
+  '3Q23',
+  '4Q23'
+];
+
 class StockService {
   constructor() {
     this.apiClient = axios.create({
-      baseURL: 'https://sheetdb.io/api/v1/8duarx5cobu6v'
+      // baseURL: 'https://sheetdb.io/api/v1/8duarx5cobu6v'
+      baseURL: 'https://sheetdb.io/api/v1/7nxifpgr4sy4b'
     });
   }
 
@@ -15,6 +31,11 @@ class StockService {
       console.error('Error fetching data:', error);
       return [];
     }
+  }
+
+  async getRevenue(position, sheetName) {
+    const data = await this.fetchData(sheetName);
+    return order.map(key => data[position][key]);
   }
 }
 
